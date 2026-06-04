8184ade63b6ffde507ce8dc187bfca89fc642a57
omar akhtar <oakhtar@d2l.com>
2026-06-04 00:37:49 -0400

    Update run_postprocess.bat

--- local_tools/robust_etl_artifact_workflow/run_postprocess.bat
+++ local_tools/robust_etl_artifact_workflow/run_postprocess.bat
@@ -1,1 +1,15 @@
-TBD
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+echo starting robust etl postprocessor...
+echo.
+
+if exist ".venv\Scripts\activate.bat" (
+    call ".venv\Scripts\activate.bat"
+)
+
+streamlit run postprocessv8.py --server.maxUploadSize 10000
+
+endlocal
