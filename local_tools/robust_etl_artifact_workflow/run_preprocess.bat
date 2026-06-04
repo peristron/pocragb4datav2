f7575fbce45d796e1ec683965648a18e55774174
omar akhtar <oakhtar@d2l.com>
2026-06-04 00:35:37 -0400

    Update run_preprocess.bat

--- local_tools/robust_etl_artifact_workflow/run_preprocess.bat
+++ local_tools/robust_etl_artifact_workflow/run_preprocess.bat
@@ -1,1 +1,15 @@
-TBD
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+echo starting robust etl preprocessor...
+echo.
+
+if exist ".venv\Scripts\activate.bat" (
+    call ".venv\Scripts\activate.bat"
+)
+
+streamlit run preprocessv11.py --server.maxUploadSize 10000
+
+endlocal
